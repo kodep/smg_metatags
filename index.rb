06ce0745e66c749f6d1a8cee54b19bcde6135b17
Andrei Malyshev <Andrei.Malyshev@kodep.ru>
2017-03-27 12:23:23 +0300

    add default get request

--- index.rb
+++ index.rb
@@ -8,12 +8,18 @@ require 'yaml'
 routes_info = YAML.load(File.read('meta_tag_content.yaml'))
 
 routes_info.each do |route, info|
-
   get "/#{route}" do
     info = routes_info[[route, params[:direct_link]].join('/')] if params[:direct_link]
     @title = info['Title']
     @description = info['Description']
     erb :'index'
   end
-
 end
+
+
+get '/*' do
+  @title = 'SWAYY | Targeted Influencer Marketing for Hotels & Restaurants'
+  @description = 'Swayy is the world is first hotel and restaurant booking site for digital Influencers. Swayy helps Leisure Businesses increase sales and brand awareness through effective Influencer Marketing.'
+  erb :'index'
+end
+
